bfd97eddc510d8576511c641c5fe2ac529e7902a
ethanphp <111652402+ethanphp@users.noreply.github.com>
2022-08-20 17:42:12 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,8 +2,8 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 const config = {
-   	apiKey: AIzaSyCIdHip8_VPD3OLWdTqSp06twbxIBqeJsot,
-  	authDomain: projectq-628bb.firebaseapp.com,
+   	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
